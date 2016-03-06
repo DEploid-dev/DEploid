8f193b7b3ea9f9344f0859a4db61ab88b3b27fe5
Joe Zhu <sha.joe.zhu@gmail.com>
2016-03-06 22:15:16 +0000

    plot llk plots, and report llk dic

--- labStrains/makePlots.r
+++ labStrains/makePlots.r
@@ -1,4 +1,25 @@
-for (i in 1:30){
+rm(list= ls())
+
+fun.llk<-function(cov.ref, cov.alt, f.samp, err=0.01, fac=100) {
+    f.samp<-f.samp+err*(1-2*f.samp);
+    llk<-lbeta(cov.alt+f.samp*fac, cov.ref+(1-f.samp)*fac)-lbeta(f.samp*fac,(1-f.samp)*fac);
+    if (sum(is.nan(llk))>1){
+      print("f.samp = ")
+    }
+#    llk<-lgamma(fac*f.samp+cov.alt)+lgamma(fac*(1-f.samp)+cov.ref)-lgamma(fac*f.samp)-lgamma(fac*(1-f.samp));
+    return(llk);
+}
+
+fun.dic.by.llk.var = function ( tmpllk ){
+     return (  mean(-2*tmpllk) + var(-2*tmpllk)/2 )# D_bar + 1/2 var (D_theta), where D_theta = -2*tmpllk, and D_bar = mean(D_theta)
+}
+
+fun.dic.by.theta = function ( tmpllk, thetallk ){
+    DIC.WSAF.bar = -2 * sum(thetallk)
+    return (  mean(-2*tmpllk) + (mean(-2*tmpllk) - DIC.WSAF.bar) ) # D_bar + pD, where pD = D_bar - D_theta, and D_bar = mean(D_theta)
+}
+
+for (i in 1:15){
      dataDir = "../"
      currentDir = system("echo ${PWD##*/}", intern = T)
 
@@ -41,7 +62,11 @@ for (i in 1:30){
      llkEvent = llkTable$V1
      llk_sd = sd(llk)
      llk_range = range(llk)
-     plot(llk, lty=2, type="l", col="black", xlab="Iteration", ylab="LLK", main=paste("LLK sd:", round(llk_sd, digits = 4),", range:",round(llk_range[1]),",",round(llk_range[2])));
+     dic.by.var = fun.dic.by.llk.var (llk)
+     dic.by.theta = fun.dic.by.theta ( llk, fun.llk(ref, alt, expWSAF))
+     plot(llk, lty=2, type="l", col="black", xlab="Iteration", ylab="LLK", main=paste("LLK sd:", round(llk_sd, digits = 4),
+                                                                                      ", dic.by.var: ",round(dic.by.var),
+                                                                                      ", dic.by.theta: ",round(dic.by.theta)));
      updateSingleAt = which(llkEvent == 1)
      updateBothAt = which(llkEvent == 2)
      updatePropAt = which(llkEvent == 0)
