fa3ae964ba9ddcbc98362c057e6dd3ab2bb0e2db
Joe Zhu <sha.joe.zhu@gmail.com>
2016-03-14 00:18:21 +0000

    rework on making plots close #54

--- utilities/makePlots.r
+++ utilities/makePlots.r
@@ -1,46 +1,99 @@
-for (i in 1:30){
-     currentDir = system("echo ${PWD##*/}", intern = T)
-
-     prefix = paste(currentDir,"_seed", i, sep="")
-     #prefix = "PG0394_ind_seed1"
-     png(paste( prefix, ".prop.png", sep= ""))
-     tmpProp = read.table(paste(prefix,".prop",sep=""), header=F)
-     rainbowColorBin = 12
-     barplot(t(tmpProp), beside=F, border=NA, col=rainbow(rainbowColorBin), space=0, xlab="Iteration", ylab="Component Freq", main="Components")
-     dev.off()
-
-     ref = read.table(paste("../../",currentDir,"_ref.txt",sep = ""), header = T)[,3]
-     alt = read.table(paste("../../",currentDir,"_alt.txt",sep = ""), header = T)[,3]
-
-     prop = as.numeric(tmpProp[dim(tmpProp)[1],])
-     hap = as.matrix(read.table(paste(prefix,".hap",sep=""), header=F))
-
-     obsWSAF = alt/(alt+ref+0.00000001)
-     expWSAF = hap %*%prop
-
-     currentWSAFcov  = cov(obsWSAF, expWSAF)
-     currentWSAFcorr = cor(obsWSAF, expWSAF)
-     png(paste( prefix, ".wsaf.png", sep= ""))
-
-     plot(obsWSAF, expWSAF, pch=19, col="blue", xlab="Observed WSAF (ALT/(ALT+REF))", ylab="Expected WSAF (h%*%p)",
-          main=paste("Sample Freq (cov =",format(currentWSAFcov,digits=4), "corr = ", format(currentWSAFcorr,digits=4),")"),
-          xlim = c(-0.05, 1.05), cex = 0.5, ylim = c(-0.05, 1.05));
-     abline(0,1,lty="dotted");
-     dev.off()
-
-     png(paste( prefix, ".llk.png", sep= ""))
-     llkTable = read.table( paste( prefix, ".llk", sep=""), header=F)
-     llk = llkTable$V2
-     llkEvent = llkTable$V1
-     llk_sd = sd(llk)
-     llk_range = range(llk)
-     plot(llk, lty=2, type="l", col="black", xlab="Iteration", ylab="LLK", main=paste("LLK sd:", round(llk_sd, digits = 4),", range:",round(llk_range[1]),",",round(llk_range[2])));
-     updateSingleAt = which(llkEvent == 1)
-     updateBothAt = which(llkEvent == 2)
-     updatePropAt = which(llkEvent == 0)
-     index = c(1:length(llk))
-     points(index[updateSingleAt], llk[updateSingleAt], cex = 0.6, col="red")
-     points(index[updateBothAt], llk[updateBothAt], cex = 0.6, col="blue")
-     points(index[updatePropAt], llk[updatePropAt], cex = 0.6, col="green")
-     dev.off()
+rm(list= ls())
+#R --slave "--args prefix refPath altPath plafPath excludePath" < makePlots.r > plotLLK.suffix.rout
+# R --slave "--args PG0389.C.excluded.noPanel.seed1 labStrains/PG0389.C_ref.txt labStrains/PG0389.C_alt.txt labStrains/labStrains_samples_PLAF.txt labStrains/PG0389.C.exclude.csv " < utilities/makePlots.r
+# R --slave "--args PG0389.C.noPanel.seed1 labStrains/PG0389.C_ref.txt labStrains/PG0389.C_alt.txt labStrains/labStrains_samples_PLAF.txt " < utilities/makePlots.r
+# R --slave "--args PG0389.C.excluded.panel.seed1 labStrains/PG0389.C_ref.txt labStrains/PG0389.C_alt.txt labStrains/labStrains_samples_PLAF.txt labStrains/PG0389.C.exclude.csv "< makePlots.r
+fun.llk<-function(cov.ref, cov.alt, f.samp, err=0.01, fac=100) {
+    f.samp<-f.samp+err*(1-2*f.samp);
+    llk<-lbeta(cov.alt+f.samp*fac, cov.ref+(1-f.samp)*fac)-lbeta(f.samp*fac,(1-f.samp)*fac);
+    if (sum(is.nan(llk))>1){
+      print("f.samp = ")
+    }
+#    llk<-lgamma(fac*f.samp+cov.alt)+lgamma(fac*(1-f.samp)+cov.ref)-lgamma(fac*f.samp)-lgamma(fac*(1-f.samp));
+    return(llk);
 }
+
+fun.dic.by.llk.var = function ( tmpllk ){
+     return (  mean(-2*tmpllk) + var(-2*tmpllk)/2 )# D_bar + 1/2 var (D_theta), where D_theta = -2*tmpllk, and D_bar = mean(D_theta)
+}
+
+fun.dic.by.theta = function ( tmpllk, thetallk ){
+    DIC.WSAF.bar = -2 * sum(thetallk)
+    return (  mean(-2*tmpllk) + (mean(-2*tmpllk) - DIC.WSAF.bar) ) # D_bar + pD, where pD = D_bar - D_theta, and D_bar = mean(D_theta)
+}
+
+args=(commandArgs(TRUE))
+print(args)
+
+prefix = args[1]
+refFile = args[2]
+altFile = args[3]
+ref.full = read.table(refFile, header = T)
+alt.full = read.table(altFile, header = T)
+
+plafFile = args[4]
+
+if ( length(args) == 5 ){
+    excludeFile = args[5]
+    exclude = read.csv(excludeFile, header = T)
+    index = which(! paste(ref.full$CHROM, ref.full$POS) %in% paste(exclude$CHROM, exclude$POS))
+    ref = ref.full[index,3]
+    alt = alt.full[index,3]
+    plaf = read.table(plafFile, header = T)[index,3]
+} else {
+    ref = ref.full[,3]
+    alt = alt.full[,3]
+    plaf = read.table(plafFile, header = T)[,3]
+}
+
+unlink ( paste( prefix, "*.png", sep= ""))
+
+png(paste( prefix, ".png", sep= ""), width = 1000, height = 1000)
+
+par (mfrow = c(2,2))
+
+tmpProp = read.table(paste(prefix,".prop",sep=""), header=F)
+rainbowColorBin = 12
+barplot(t(tmpProp), beside=F, border=NA, col=rainbow(rainbowColorBin), space=0, xlab="Iteration", ylab="Component Freq", main="Components")
+
+
+prop = as.numeric(tmpProp[dim(tmpProp)[1],])
+hap = as.matrix(read.table(paste(prefix,".hap",sep=""), header=T)[,-c(1,2)] )
+
+obsWSAF = alt/(alt+ref+0.00000001)
+expWSAF = hap %*%prop
+
+currentWSAFcov  = cov(obsWSAF, expWSAF)
+currentWSAFcorr = cor(obsWSAF, expWSAF)
+
+plot(obsWSAF, expWSAF, pch=19, col="blue", xlab="Observed WSAF (ALT/(ALT+REF))", ylab="Expected WSAF (h%*%p)",
+     main=paste("Sample Freq (cov =",format(currentWSAFcov,digits=4), "corr = ", format(currentWSAFcorr,digits=4),")"),
+     xlim = c(-0.05, 1.05), cex = 0.5, ylim = c(-0.05, 1.05));
+abline(0,1,lty="dotted");
+
+plot ( plaf, obsWSAF, col = "red")
+points ( plaf, expWSAF, col = "blue")
+
+llkTable = read.table( paste( prefix, ".llk", sep=""), header=F)
+llk = llkTable$V2
+llkEvent = llkTable$V1
+llk_sd = sd(llk)
+llk_range = range(llk)
+dic.by.var = fun.dic.by.llk.var (llk)
+dic.by.theta = fun.dic.by.theta ( llk, fun.llk(ref, alt, expWSAF))
+plot(llk, lty=2, type="l", col="black", xlab="Iteration", ylab="LLK", main=paste("LLK sd:", round(llk_sd, digits = 4),
+                                                                                 ", dic.by.var: ",round(dic.by.var),
+                                                                                 ", dic.by.theta: ",round(dic.by.theta)));
+updateSingleAt = which(llkEvent == 1)
+updateBothAt = which(llkEvent == 2)
+updatePropAt = which(llkEvent == 0)
+index = c(1:length(llk))
+points(index[updateSingleAt], llk[updateSingleAt], cex = 0.6, col="red")
+points(index[updateBothAt], llk[updateBothAt], cex = 0.6, col="blue")
+points(index[updatePropAt], llk[updatePropAt], cex = 0.6, col="green")
+dev.off()
+
+logFileName = paste( prefix, "dic.log", sep = "")
+cat ( "corr: ",  currentWSAFcorr, "\n", file = logFileName)
+cat ( "dic.by.var: ", dic.by.var, "\n", file = logFileName, append = T)
+cat ( "dic.by.theta: ", dic.by.theta, "\n", file = logFileName, append = T)
