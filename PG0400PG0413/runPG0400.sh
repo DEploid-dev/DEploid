44bfd6ae7272d7256de92233cb4185b232563e73
Joe Zhu <sha.joe.zhu@gmail.com>
2016-03-17 17:39:03 +0000

    cluster PG0400 to check

--- PG0400PG0413/runPG0400.sh
+++ PG0400PG0413/runPG0400.sh
@@ -5,10 +5,11 @@
 #$ -e ErrFiles
 #$ -o OutFiles
 #$ -N PG0400
-#$ -t 1-30
+#$ -t 1-6
 
 sample="PG0400.C"
 root="/well/mcvean/joezhu/pf3k/pf3k_5_1/labStrains/"
+#root="../labStrains/"
 
 ref=${root}${sample}_ref.txt
 alt=${root}${sample}_alt.txt
@@ -17,6 +18,34 @@ panel=${root}Hb3_7g8.csv
 seed=${SGE_TASK_ID}
 
 exclude=${sample}.exclude.csv
+
+prefix=${root}${sample}/${sample}_exclude_panelOff_seed${seed}
+
+time pfDeconv -ref ${ref} \
+-alt ${alt} \
+-plaf  ${plaf} \
+-noPanel \
+-seed ${seed} \
+-exclude ${exclude} \
+-o ${prefix} \
+-burn 0
+
+R --slave "--args ${prefix} ${ref} ${alt} ${plaf} ${exclude}" < ../utilities/makePlots.r
+
+prefix=${root}${sample}/${sample}_exclude_panel_seed${seed}miss0recom1
+
+time pfDeconv -ref ${ref} \
+-alt ${alt} \
+-plaf  ${plaf} \
+-panel ${panel} \
+-seed ${seed} \
+-exclude ${exclude} \
+-o ${prefix} \
+-burn 0 \
+-miss 0 \
+-recomb 1
+
+R --slave "--args ${prefix} ${ref} ${alt} ${plaf} ${exclude}" < ../utilities/makePlots.r
 
 prefix=${root}${sample}/${sample}_exclude_panel_seed${seed}
 
@@ -41,6 +70,7 @@ time pfDeconv -ref ${ref} \
 -seed ${seed} \
 -exclude ${exclude} \
 -o ${prefix} \
+-burn 0 \
 -miss 0.01 \
 -recomb 0.01
 
@@ -55,24 +85,11 @@ time pfDeconv -ref ${ref} \
 -seed ${seed} \
 -exclude ${exclude} \
 -o ${prefix} \
+-burn 0 \
 -miss 0.1 \
 -recomb 0.1
 
 R --slave "--args ${prefix} ${ref} ${alt} ${plaf} ${exclude}" < ../utilities/makePlots.r
 
 
-prefix=${root}${sample}/${sample}_exclude_panel_seed${seed}miss0recom1
-
-time pfDeconv -ref ${ref} \
--alt ${alt} \
--plaf  ${plaf} \
--panel ${panel} \
--seed ${seed} \
--exclude ${exclude} \
--o ${prefix} \
--burn 0 \
--miss 0 \
--recomb 1
-
-R --slave "--args ${prefix} ${ref} ${alt} ${plaf} ${exclude}" < ../utilities/makePlots.r
 
