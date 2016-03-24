2e23b73a49dbffc970aff090946afb3a6cb2a8a4
Joe Zhu <sha.joe.zhu@gmail.com>
2016-03-24 22:56:08 +0000

    rearrange random generator related files

--- tests/test-coverage-local.sh
+++ tests/test-coverage-local.sh
@@ -6,7 +6,7 @@
 # Generate html report
 #lcov --base-directory . --directory . --zerocounters -q
 make check -mj
-#lcov --base-directory . --directory . -c -o libbash_test.info
-lcov --remove libbash_test.info "/usr*" "src/codeCogs/*" -o libbash_test.info # remove output for external libraries
+lcov --base-directory . --directory . -c -o libbash_test.info
+lcov --remove libbash_test.info "/usr*" "src/codeCogs/*" "src/random/*" -o libbash_test.info # remove output for external libraries
 rm -rf ./test_coverage
 genhtml -o ./test_coverage -t "libbash test coverage" --num-spaces 4 libbash_test.info
