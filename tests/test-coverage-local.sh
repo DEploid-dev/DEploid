56fd7f80c0ee43e6b4e86e0ef9b4986a214c2432
Joe Zhu <sha.joe.zhu@gmail.com>
2016-03-28 13:07:14 +0100

    --demangle-cpp output

--- tests/test-coverage-local.sh
+++ tests/test-coverage-local.sh
@@ -4,9 +4,10 @@
 #make -mj
 
 # Generate html report
-#lcov --base-directory . --directory . --zerocounters -q
+lcov --base-directory . --directory . --zerocounters -q
 make check -mj
 lcov --base-directory . --directory . -c -o libbash_test.info
+# --rc lcov_branch_coverage=1 option will turn on branch check
 lcov --remove libbash_test.info "/usr*" "src/codeCogs/*" "src/random/*" -o libbash_test.info # remove output for external libraries
 rm -rf ./test_coverage
-genhtml -o ./test_coverage -t "libbash test coverage" --num-spaces 4 libbash_test.info
+genhtml -o ./test_coverage -t "libbash test coverage" --num-spaces 4 libbash_test.info --legend --function-coverage --demangle-cpp
