62124c296f2bc8e254c15d7f46e40e3418dd4b37
Joe Zhu <sha.joe.zhu@gmail.com>
2016-06-20 12:59:50 +0100

    update plotting utilities

--- utilities/makePlots2.r
+++ utilities/makePlots2.r
@@ -1,37 +1,26 @@
-rm(list= ls()
+rm(list= ls())
 
 plot.prob <-function (tmpProp, title){
     rainbowColorBin = 16
-#    rainbowColorBin = 5
     barplot(t(tmpProp), beside=F, border=NA, col=rainbow(rainbowColorBin), space=0, xlab="SNP index", ylab="Posterior probabilities", main=title)
 }
 
+plot.postProb.ofCase <- function ( case ){
+    png(paste(prefix, ".", case, ".png", sep = ""), width = 3500, height = 2000)
+    obj = read.table( paste(prefix, ".", case, sep = ""), header=T)
+    chromName = levels(obj$CHROM)
+    par(mfrow = c(length(chromName)/2,2))
+    for ( chromI in chromName ){
+        plot.prob ( obj[which( chromI == obj$CHROM),c(3:dim(obj)[2])], "")
+    }
+    dev.off()
+}
+
+args=(commandArgs(TRUE))
+
 prefix = args[1]
 
-png(paste(prefix, ".single1.png", sep = ""), width = 3500, height = 2000)
-single1 = read.table( paste(prefix, ".single1", sep = ""), header=T)
-chromName = levels(single1$CHROM)
-par(mfrow = c(length(chromName)/2,2))
-for ( chromI in chromName ){
-    plot.prob ( single1[which( chromI == single1$CHROM),c(3:dim(single1)[2])], "")
-}
-dev.off()
+plot.postProb.ofCase("single1")
+plot.postProb.ofCase("single2")
+plot.postProb.ofCase("pair")
 
-
-png(paste(prefix, ".single2.png", sep = ""), width = 3500, height = 2000)
-single2 = read.table( paste(prefix, ".single2", sep = ""), header=T)
-chromName = levels(single2$CHROM)
-par(mfrow = c(length(chromName)/2,2))
-for ( chromI in chromName ){
-    plot.prob ( single2[which( chromI == single2$CHROM),c(3:dim(single2)[2])], "")
-}
-
-
-png(paste(prefix, ".pair.png", sep = ""), width = 3500, height = 2000)
-pair = read.table( paste(prefix, ".pair", sep = ""), header=T)
-chromName = levels(pair$CHROM)
-par(mfrow = c(length(chromName)/2,2))
-for ( chromI in chromName ){
-    plot.prob ( pair[which( chromI == pair$CHROM),c(3:dim(pair)[2])], "")
-}
-dev.off()
