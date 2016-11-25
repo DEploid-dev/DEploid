44d5d887575fa08fc8c6af171da6051c64c4552f
Joe Zhu <sha.joe.zhu@gmail.com>
2016-11-25 13:06:15 +0000

    include enhanced alt vs ref plot

--- utilities/DEploidR.R
+++ utilities/DEploidR.R
@@ -176,10 +176,22 @@ plotProportions <- function (proportions, title = "Components"){
 #'
 plotAltVsRef <- function ( ref, alt, title = "Alt vs Ref",
                     exclude.ref = c(), exclude.alt = c() ){
+    cr <- colorRampPalette(colors=c("#de2d26","#2b8cbe"))
+    colors <- cr(31)
+    ratios <- ref/(ref+alt+0.0000001)
     tmpRange <- 1.1 * mean(max(alt), max(ref))
     plot ( ref, alt, xlim = c(0, tmpRange), ylim = c(0, tmpRange),
-        cex = 0.5, xlab = "REF", ylab = "ALT", main = title, cex.lab = 2.5,
-        cex.main = 2.5, cex.axis = 2.5)
+        pch=20, col=scales::alpha(colors[ceiling(ratios * 30)+1], 0.7),
+        xlab="Reference # Reads", ylab="Alternative # Reads", main=title,
+        cex = 0.5, cex.lab = 2.5, cex.main = 2.5, cex.axis = 2.5)
+    legend("topright", legend=c("100% Ref","100% Alt", "50/50"),
+        fill=colors[c(1,31,15)], cex=2.5, border=NA, box.lwd=0, box.col="white",
+        bg = NA)
+    abline(a=0,b=1, lwd=2, lty=2, col="gray")
+
+#    plot ( ref, alt, xlim = c(0, tmpRange), ylim = c(0, tmpRange),
+#        cex = 0.5, xlab = "REF", ylab = "ALT", main = title, cex.lab = 2.5,
+#        cex.main = 2.5, cex.axis = 2.5)
     points (exclude.ref, exclude.alt, col = "red")
     abline(v = 50, untf = FALSE, lty = 2)
     abline(h = 50, untf = FALSE, lty = 2)
