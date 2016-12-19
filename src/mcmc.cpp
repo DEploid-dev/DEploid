c8db37eee88a61d3cb9c3c270673699b7c531d07
Joe Zhu <sha.joe.zhu@gmail.com>
2016-12-19 18:03:46 +0000

    fixing the seg fault

--- src/mcmc.cpp
+++ src/mcmc.cpp
@@ -90,7 +90,7 @@ void McmcMachinery::initializeMcmcChain( ){
     this->initializeExpectedWsaf(); // This requires currentHap_ and currentProp_
     this->currentLLks_ = calcLLKs( this->dEploidIO_->refCount_, this->dEploidIO_->altCount_, this->currentExpectedWsaf_ , 0, this->currentExpectedWsaf_.size());
 
-    if ( this->dEploidIO_->doAllowInbreeding() != true ){
+    if ( this->dEploidIO_->doAllowInbreeding() == true ){
         this->intializeUpdateReferencePanel(this->panel_->truePanelSize()+kStrain_-1);
     }
 
@@ -143,9 +143,9 @@ void McmcMachinery::updateReferencePanel(size_t inbreedingPanelSizeSetTo, size_t
         return;
     }
 
-    if ( this->dEploidIO_->doAllowInbreeding() != true ){
-        return;
-    }
+    //if ( this->dEploidIO_->doAllowInbreeding() != true ){
+        //return;
+    //}
 
     this->panel_->setInbreedingPanelSize(inbreedingPanelSizeSetTo);
     //this->panel_->setInbreedingPanelSize( this->panel_->truePanelSize() + this->kStrain_ - 2);
@@ -364,7 +364,7 @@ void McmcMachinery::updateSingleHap(){
     dout << " Update Single Hap "<<endl;
     this->findUpdatingStrainSingle();
 
-    if ( this->dEploidIO_->doAllowInbreeding() != true ){
+    if ( this->dEploidIO_->doAllowInbreeding() == true ){
         this->updateReferencePanel(this->panel_->truePanelSize()+kStrain_-1, this->strainIndex_);
     }
 
@@ -381,7 +381,7 @@ void McmcMachinery::updateSingleHap(){
                                   this->panel_, this->dEploidIO_->missCopyProb_,
                                   this->strainIndex_);
 
-        if ( this->dEploidIO_->doAllowInbreeding() != true ){
+        if ( this->dEploidIO_->doAllowInbreeding() == true ){
             updating.setPanelSize(this->panel_->inbreedingPanelSize());
         }
 
