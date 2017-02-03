09210ef92fd3fd3ec6a48df2374532e985b830e8
Joe Zhu <sha.joe.zhu@gmail.com>
2017-02-03 18:00:28 +0000

    plot skiping the first plot

--- utilities/interpretDEploid.r
+++ utilities/interpretDEploid.r
@@ -25,7 +25,9 @@ myPlafInfo = extractPLAF( myInput$plafFileName )
 
 myExcludeInfo = fun.extract.exclude (myInput$excludeFileName, myInput$excludeBool)
 
-fun.interpretDEploid.1 (myCoverageInfo, myPlafInfo, myInput$dEploidPrefix, myInput$outPrefix, myExcludeInfo, myInput$pdfBool)
+if ( myInput$skip1Bool == FALSE ){
+    fun.interpretDEploid.1 (myCoverageInfo, myPlafInfo, myInput$dEploidPrefix, myInput$outPrefix, myExcludeInfo, myInput$pdfBool)
+}
 
 fun.interpretDEploid.2 (myCoverageInfo, myInput$dEploidPrefix, myInput$outPrefix, myExcludeInfo, myInput$pdfBool)
 
