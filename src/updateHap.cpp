c279601e272e8d70c8da42efe7a0c21aa2eec5e0
Joe Zhu <sha.joe.zhu@gmail.com>
2017-04-21 17:50:55 +0100

    fixing bckwd alg

--- src/updateHap.cpp
+++ src/updateHap.cpp
@@ -154,9 +154,9 @@ void UpdateSingleHap::calcBwdProbs(){
         for ( size_t i = 0 ; i < this->nPanel_; i++){
             bwdTmp[i] = 0.0;
             for ( size_t ii = 0 ; ii < this->nPanel_; ii++){
-                bwdTmp[i] += this->emission_[j][this->panel_->content_[hapIndexBack][ii]] * bwdProbs_.back()[i] * pRecEachHap;
+                bwdTmp[i] += this->emission_[j][this->panel_->content_[hapIndexBack][ii]] * bwdProbs_.back()[ii] * pRecEachHap;
                 if ( i == ii){
-                    bwdTmp[i] += this->emission_[j][this->panel_->content_[hapIndexBack][ii]] * bwdProbs_.back()[i] * pNoRec;
+                    bwdTmp[i] += this->emission_[j][this->panel_->content_[hapIndexBack][ii]] * bwdProbs_.back()[ii] * pNoRec;
                 }
             }
         }
