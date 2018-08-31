e7947ec647dd8f00537c1cadf4e3e3278e4fc25d
Joe Zhu <sha.joe.zhu@gmail.com>
2018-09-01 00:45:41 +0100

    update style check

--- .ci/style.sh
+++ .ci/style.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for file in $(ls src/); do
-  cpplint --filter=-build/include_subdir src/${file} 2>&1 > lint_tmp
+
+while read file; do
+  cpplint --filter=-build/include_subdir ${file} 2>&1 > lint_tmp
   if [ $? -ne 0 ]; then
     echo "Coding style check:"
     echo "cpplint ${file} failed"
     cat lint_tmp
     exit 1
   fi
-done
+done < .ci/checkedList
+
+echo "Coding style check: PASS"
