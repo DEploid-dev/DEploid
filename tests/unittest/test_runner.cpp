680f254722fdd5e341e42a0c0f4d434f201255b7
Joe Zhu <sha.joe.zhu@gmail.com>
2020-12-29 15:02:30 +0000

    update check style

--- tests/unittest/test_runner.cpp
+++ tests/unittest/test_runner.cpp
@@ -1,8 +1,28 @@
 /*
- * Execute this program to run the test suite.
+ * dEploid is used for deconvoluting Plasmodium falciparum genome from
+ * mix-infected patient sample.
  *
- * Usually there's no need to change anything in here.
+ * Copyright (C) 2016-2017 University of Oxford
+ *
+ * Author: Sha (Joe) Zhu
+ *
+ * This file is part of dEploid.
+ *
+ * dEploid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/TestRunner.h>
@@ -10,22 +30,21 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/CompilerOutputter.h>
 
-using namespace CppUnit;
 
 int main(void) {
-    TestResult controller;
+    CppUnit::TestResult controller;
 
-    TestResultCollector result;
+    CppUnit::TestResultCollector result;
     controller.addListener(&result);
 
-    BriefTestProgressListener progress;
+    CppUnit::BriefTestProgressListener progress;
     controller.addListener(&progress);
 
-    TestRunner runner;
-    runner.addTest( TestFactoryRegistry::getRegistry().makeTest() );
+    CppUnit::TestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
     runner.run(controller);
 
-    CompilerOutputter outputter(&result, std::cerr);
+    CppUnit::CompilerOutputter outputter(&result, std::cerr);
     outputter.write();
 
     return result.wasSuccessful() ? 0 : 1;
