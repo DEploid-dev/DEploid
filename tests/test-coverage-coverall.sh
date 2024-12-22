741aaa3bba1bf657f63871e5ac05218f1347c0c6
Joe Zhu <sha.joe.zhu@gmail.com>
2024-12-23 00:45:47 +0800

    test again

--- tests/test-coverage-coverall.sh
+++ tests/test-coverage-coverall.sh
@@ -4,8 +4,8 @@
 make -mj
 
 # Generate html report
-lcov --ignore-errors empty --base-directory . --directory . --zerocounters -q
+lcov --ignore-errors version --base-directory . --directory . --zerocounters -q
 make check -mj
-lcov --ignore-errors empty --base-directory . --directory . -c -o coverage/lcov.info
+lcov --ignore-errors version --base-directory . --directory . -c -o coverage/lcov.info
 # --rc lcov_branch_coverage=1 option will turn on branch check
-lcov --ignore-errors empty --remove coverage/lcov.info "/usr*" "src/codeCogs/*" "src/vcf/*" "src/lasso/*" "src/export/*" "src/gzstream/*" "tests/unittest/*" -o coverage/lcov.info # remove output for external libraries
+lcov --ignore-errors version --remove coverage/lcov.info "/usr*" "src/codeCogs/*" "src/vcf/*" "src/lasso/*" "src/export/*" "src/gzstream/*" "tests/unittest/*" -o coverage/lcov.info # remove output for external libraries
